c9d5ca1020126a9bd2e1bd95c297c74e5c055c9c
sunilingole <sunilingole1196@gmail.com>
2021-11-26 04:17:59 -0800

    changes in employee file

--- SpringBootHibernateSwagger/SpringBootHibernateSwagger/src/main/java/com/csi/model/Employee.java
+++ SpringBootHibernateSwagger/SpringBootHibernateSwagger/src/main/java/com/csi/model/Employee.java
@@ -1,20 +1,20 @@
 package com.csi.model;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+//Sunil Ingole means Golya
 @Entity
 @Data
 public class Employee {
-    @Id
-    @GeneratedValue
-    private int empId;
-    private String empName;
+	@Id
+	@GeneratedValue
+	private int empId;
+	private String empName;
 
-
-    private String empEmailId;
-    private String empPassword;
+	private String empEmailId;
+	private String empPassword;
 }
